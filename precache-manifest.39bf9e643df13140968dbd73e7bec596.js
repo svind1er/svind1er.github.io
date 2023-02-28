aecaa638e86000bdf13301ca9153f0652fe5bbb4
svind1er <rusher10891@gmail.com>
2023-02-28 02:37:10 +0100

    Updates

---
+++ precache-manifest.39bf9e643df13140968dbd73e7bec596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d52ef7d4057b134678257acb51080a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caadd369777d3edb62f1",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2a38066e7acc4a613ee3",
+    "url": "./static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "caadd369777d3edb62f1",
+    "url": "./static/js/2.7e3fe0ed.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.7e3fe0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a38066e7acc4a613ee3",
+    "url": "./static/js/main.a48ac670.chunk.js"
+  },
+  {
+    "revision": "066e1fe302588d5b0de2",
+    "url": "./static/js/runtime-main.560b22e6.js"
+  }
+]);
